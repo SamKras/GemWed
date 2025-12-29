de35cdb827715cb96ea35e28d4e027fdda383acb
SamKras <krasilniksem@gmail.com>
2025-12-29 15:49:49 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // ДОБАВЬТЕ ЭТУ СТРОКУ:
-      base: './', 
-      
+      base: './', // Чтобы пути к файлам всегда находились
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || ""),
+        'process.env': {} // ЭТО ОБЯЗАТЕЛЬНО: лечит ошибку "process is not defined"
       },
       resolve: {
         alias: {
@@ -23,4 +21,3 @@ export default defineConfig(({ mode }) => {
         }
       }
     };
-});
